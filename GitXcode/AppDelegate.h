e25cd2681b62761884c478bea85ac613ef0ff7cd
cnobleart <cnobleart@att.net>
2016-12-31 11:27:13 -0600

    Initial Commit

---
+++ GitXcode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitXcode
+//
+//  Created by Carol M Noble on 12/31/16.
+//  Copyright © 2016 Carol M Noble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitXcode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitXcode
+//
+//  Created by Carol M Noble on 12/31/16.
+//  Copyright © 2016 Carol M Noble. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
